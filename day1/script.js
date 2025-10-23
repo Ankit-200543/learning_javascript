4e5ad04e3cc2d454d97418eaeb8b5c7ffccbe3aa
Ankit-200543 <ak8946562@gmail.com>
2025-10-23 22:32:43 +0530

    variable

--- day1/script.js
+++
@@ -1,8 +0,0 @@
-//print
-console.log("hello");
-
-//variable
-let a=10
-var b=25
-const ok=90
-console.log(" "+a+" "+ b+" "+ok)
---
+++ var&&dataTypes.js
@@ -0,0 +1,42 @@
+//print
+console.log("hello");
+
+
+//variable
+let userName="ankit";
+let a;
+console.log(a);
+//undefined
+
+
+{
+    let p=10
+    console.log(p)
+}
+// console.log(p)
+//error because let and const have scope only in a specific Block
+
+//<-------datatypes---->
+//->primitive dataTypes
+
+//Number.String,Boolean,Undefined,Null,BigInt,Symbol
+
+//->Non-Primitive dataTypes
+
+//Objects:-collection of key:value pairs
+const student={
+    FullName:"Ankit Kumar",
+    age:20
+};
+console.log(student.FullName)
+student["age"]=25;
+
+console.log(student.age)
+
+
+
+
+
+
+
+
